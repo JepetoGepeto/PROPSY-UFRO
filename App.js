33e945fe0132b68ec8894419045b6ab710d29fde
Nicolas <nicolasfelipegp@gmail.com>
2023-07-16 19:00:45 -0400

    dispatch

--- App.js
+++ App.js
@@ -1,24 +1,34 @@
 import React, { useEffect } from 'react';
-import { Buffer } from "buffer";
-import NavigationStack from './src/stack/NavigationStack';
+import { Buffer } from 'buffer';
+import { Provider, useSelector, useDispatch } from 'react-redux'; // Import Provider, useSelector, and useDispatch from 'react-redux'
+import store from './src/app/store'; // Import your Redux store
 import { NavigationContainer } from '@react-navigation/native';
 import * as ScreenOrientation from 'expo-screen-orientation';
+import NavigationStack from './src/stack/NavigationStack';
+import { initializeSurveyData } from './src/app/surveyreducer'; // Import the action
 
 if (!global.Buffer) {
   global.Buffer = Buffer;
 }
 
 export default function App() {
+  const dispatch = useDispatch(); // Create a dispatch function to use the action
+
   useEffect(() => {
     async function setScreenOrientation() {
       await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
     }
     setScreenOrientation();
-  }, []);
+
+    // Dispatch the initializeSurveyData action to initialize the survey data array
+    dispatch(initializeSurveyData());
+  }, [dispatch]);
 
   return (
-    <NavigationContainer>
-      <NavigationStack />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <NavigationStack />
+      </NavigationContainer>
+    </Provider>
   );
 }
---
+++ src/app/surveyReducer.js
@@ -0,0 +1,23 @@
+// surveyReducer.js
+import { createSlice, createAction } from '@reduxjs/toolkit';
+
+const initialState = {
+  surveyData: [],
+};
+
+const surveySlice = createSlice({
+  name: 'survey',
+  initialState,
+  reducers: {
+    addAnswer: (state, action) => {
+      state.surveyData.push(action.payload);
+    },
+    // Add more reducers here if needed
+  },
+});
+
+export const { addAnswer } = surveySlice.actions;
+
+export const initializeSurveyData = createAction('survey/initializeSurveyData');
+
+export default surveySlice.reducer;
