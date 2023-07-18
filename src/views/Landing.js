a6b0dbf97b13d7dcf44c0d4644c89e745d91e5ed
Nicolas <nicolasfelipegp@gmail.com>
2023-07-18 00:06:20 -0400

    Update

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -8,10 +8,10 @@ export default function App() {
 
   return (
     <View className="flex-1 bg-[#5F6896]">
-      <View className="bg-[#FFFFFF] flex-row justify-center items-center p-20 rounded-b-[35]">
-        <Image source={require('../../assets/images/LabCES.jpg')} className="w-[296] h-[102]" />
-        <Text className="text-[#3E3E44] text-6xl mx-5">PROPSY</Text>
-        <Image source={require('../../assets/images/UFRO.png')} className="w-[296] h-[102]" />
+      <View className="bg-[#FFFFFF] flex-row justify-center items-center p-20 rounded-b-[35] ">
+        <Image source={require('../../assets/images/LabCES.jpg')} className="w-[296] h-[102] rotate-180" />
+        <Text className="text-[#3E3E44] text-6xl mx-5 rotate-180">PROPSY</Text>
+        <Image source={require('../../assets/images/UFRO.png')} className="w-[296] h-[102] rotate-180" />
       </View>
       <View className="flex-1 justify-center items-center">
         <Button text="INICIAR" onPress={() => navigation.navigate("Name")} />
--- src/views/LandingFinal.js
+++ src/views/LandingFinal.js
@@ -7,12 +7,12 @@ const LandingFinal = () => {
   return (
     <View className="flex-1 bg-[#5F6896]">
       <View className="bg-[#FFFFFF] flex-row justify-center items-center p-20 rounded-b-[35]">
-        <Image source={require('../../assets/images/LabCES.jpg')} className="w-[296] h-[102]" />
-        <Text className="text-[#3E3E44] text-6xl mx-5">PROPSY</Text>
-        <Image source={require('../../assets/images/UFRO.png')} className="w-[296] h-[102]" />
+        <Image source={require('../../assets/images/LabCES.jpg')} className="w-[296] h-[102] rotate-180" />
+        <Text className="text-[#3E3E44] text-6xl mx-5 rotate-180">Gracias por participar</Text>
+        <Image source={require('../../assets/images/UFRO.png')} className="w-[296] h-[102] rotate-180" />
       </View>
       <View className="flex-1 justify-center items-center">
-      <Text className="text-[#3E3E44] text-6xl mx-5">Datos guardados</Text>
+      <Text className="text-[#ffffff] text-6xl mx-5">Datos guardados</Text>
         </View>
     </View>
 
