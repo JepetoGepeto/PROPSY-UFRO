847835f50f8a28717b9e95a4877970b5090b2cfe
Nicolas <nicolasfelipegp@gmail.com>
2023-07-26 19:39:36 -0400

    Before testing

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -4,57 +4,46 @@ import Button from "../components/Button";
 import { useNavigation } from "@react-navigation/native";
 import * as FileSystem from 'expo-file-system';
 import XLSX from 'xlsx';
-import * as Permissions from 'expo-permissions'; // Add this import
-import * as MediaLibrary from 'expo-media-library'; // Add this import
+import * as Sharing from 'expo-sharing';
 
 export default function App() {
   const navigation = useNavigation();
 
   const createExcelFile = async () => {
     const folderName = 'Propsy';
-    const fileName = 'data.xlsx';
+    const fileName = 'propsy.xlsx';
     const folderPath = `${FileSystem.documentDirectory}${folderName}`;
     const filePath = `${folderPath}/${fileName}`;
 
     try {
-      // Create the folder if it doesn't exist
-      await FileSystem.makeDirectoryAsync(folderPath, { intermediates: true });
-
       // Check if the file already exists
       const fileExists = await FileSystem.getInfoAsync(filePath);
 
       // If the file doesn't exist, create it and write the headers
       if (!fileExists.exists) {
-        const headers = [['Nombre', 'Edad']];
+        // Create the folder if it doesn't exist
+        await FileSystem.makeDirectoryAsync(folderPath, { intermediates: true });
+        console.log('Folder created:', folderPath);
+
+        const headers = [['nombre', 'edad', 'nacimiento', 'sexo', 'coincide']];
         const worksheet = XLSX.utils.aoa_to_sheet(headers);
         const workbook = XLSX.utils.book_new();
         XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
         const excelFileContent = XLSX.write(workbook, { type: 'base64' });
         await FileSystem.writeAsStringAsync(filePath, excelFileContent, { encoding: FileSystem.EncodingType.Base64 });
-        Alert.alert('Archivo creado', `Se ha creado el archivo Excel en ${filePath}`);
-
-        // Now move the file to a publicly accessible folder (Download)
-        const perm = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
-        if (perm.status === 'granted') {
-          const asset = await MediaLibrary.createAssetAsync(filePath);
-          const album = await MediaLibrary.getAlbumAsync('Download');
-          if (album == null) {
-            await MediaLibrary.createAlbumAsync('Download', asset, false);
-          } else {
-            await MediaLibrary.addAssetsToAlbumAsync([asset], album, false);
-          }
-        } else {
-          console.log('Permission not granted for Media Library.');
-        }
+        console.log('File created:', filePath);
       }
+
+      // Share the file with the user
+      await Sharing.shareAsync(filePath);
 
       // Now you can navigate to the next screen
       navigation.navigate("Name");
     } catch (error) {
+      console.log('Error:', error);
       Alert.alert('Error', 'No se pudo crear el archivo Excel o la carpeta.');
     }
   };
-
 
   return (
     <View className="flex-1 bg-[#5F6896]">
--- src/views/LandingFinal.js
+++ src/views/LandingFinal.js
@@ -1,10 +1,265 @@
-import React from 'react';
-import {Text, View, Image } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, View, Image } from 'react-native';
 import { useSurveyContext } from '../SurveyContext';
+import * as FileSystem from 'expo-file-system';
+import XLSX from 'xlsx';
+import * as Sharing from 'expo-sharing';
+
 
 
 const LandingFinal = () => {
-  const { surveyData } = useSurveyContext(); // Acceder a los datos del contexto
+  const { surveyData } = useSurveyContext();
+
+  useEffect(() => {
+    createAndExportExcelFile();
+  }, []);
+
+  const createAndExportExcelFile = async () => {
+    const folderName = 'Propsy';
+    const fileName = 'propsy.xlsx';
+    const folderPath = `${FileSystem.documentDirectory}${folderName}`;
+    const filePath = `${folderPath}/${fileName}`;
+
+    try {
+      // Check if the file already exists
+      const fileExists = await FileSystem.getInfoAsync(filePath);
+
+      // If the file doesn't exist, create it and write the headers
+      if (!fileExists.exists) {
+        // Create the folder if it doesn't exist
+        await FileSystem.makeDirectoryAsync(folderPath, { intermediates: true });
+        console.log('Folder created:', folderPath);
+
+        const headers = ['nombre', 'edad', 'nacimiento', 'sexo', 'coincide'];
+        const worksheet = XLSX.utils.aoa_to_sheet([headers]);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+        const excelFileContent = XLSX.write(workbook, { type: 'base64' });
+        await FileSystem.writeAsStringAsync(filePath, excelFileContent, { encoding: FileSystem.EncodingType.Base64 });
+        console.log('File created:', filePath);
+      }
+
+      // Load the existing workbook and worksheet
+      const fileContent = await FileSystem.readAsStringAsync(filePath, { encoding: FileSystem.EncodingType.Base64 });
+      const workbook = XLSX.read(fileContent, { type: 'base64' });
+      const worksheet = workbook.Sheets['Sheet1'];
+
+      // Find the first empty row to add the survey data
+      const range = XLSX.utils.decode_range(worksheet['!ref']);
+      const firstEmptyRow = range.e.r + 1;
+
+      // Get the survey data from the context
+      const surveyData = useSurveyContext().surveyData;
+      const dataRow = [
+        surveyData.nombre,
+        surveyData.edad,
+        surveyData.cumpleanos,
+        surveyData.sexo,
+        surveyData.coincide,
+        surveyData.nivel_estudios,
+        surveyData.nivelCursado,
+        surveyData.ocupacion,
+        surveyData.trabajo,
+        surveyData.ocupacionActual,
+        surveyData.estadoCivil,
+        surveyData.salud,
+        surveyData.ingresos,
+        surveyData.personas_que_viven,
+        surveyData.agrupacion,
+        surveyData.diagnostico,
+        surveyData.otroEnfermedades,
+        surveyData.medicamentos,
+        surveyData.otrosMedicamentos,
+        surveyData.dementia,
+        surveyData.licencia,
+        surveyData.renovacion,
+        surveyData.idioma,
+        surveyData.idiomaExtra,
+        surveyData.nivel_socioeconomico,
+        surveyData.horas_sueno,
+        surveyData.sleeping1,
+        surveyData.sleeping2,
+        surveyData.sleeping3,
+        surveyData.sleeping4,
+        surveyData.sleeping5,
+        surveyData.consumo_tabaco,
+        surveyData.cigarrillosSemana,
+        surveyData.edadFumador,
+        surveyData.anos_fumo,
+        surveyData.aniosDejoDeFumar,
+        surveyData.cigarrillos,
+        surveyData.consumoAlcohol,
+        surveyData.tragosSemana,
+        surveyData.tragosFinDeSemana,
+        surveyData.edadInicioBeber,
+        surveyData.edadDejarBeber,
+        surveyData.anosBebio,
+        surveyData.tragos_semanales,
+        surveyData.mostly1,
+        surveyData.mostly2,
+        surveyData.mostly3,
+        surveyData.mostly4,
+        surveyData.mostly5,
+        surveyData.mostly6,
+        surveyData.mostly7,
+        surveyData.mostly8,
+        surveyData.mostly9,
+        surveyData.mostly10,
+        surveyData.mostly11,
+        surveyData.mostly12,
+        surveyData.mostly13,
+        surveyData.mostly14,
+        surveyData.mostly15,
+        surveyData.older1,
+        surveyData.older2,
+        surveyData.older3,
+        surveyData.older4,
+        surveyData.older5,
+        surveyData.older6,
+        surveyData.older7,
+        surveyData.older8,
+        surveyData.older9,
+        surveyData.older10,
+        surveyData.older11,
+        surveyData.older12,
+        surveyData.older13,
+        surveyData.feeling1,
+        surveyData.feeling2,
+        surveyData.feeling3,
+        surveyData.feeling4,
+        surveyData.feeling5,
+        surveyData.feeling6,
+        surveyData.feeling7,
+        surveyData.process1,
+        surveyData.process2,
+        surveyData.process3,
+        surveyData.process4,
+        surveyData.process5,
+        surveyData.process6,
+        surveyData.process7,
+        surveyData.process8,
+        surveyData.process9,
+        surveyData.process10,
+        surveyData.process11,
+        surveyData.process12,
+        surveyData.process13,
+        surveyData.process14,
+        surveyData.process15,
+        surveyData.process16,
+        surveyData.process17,
+        surveyData.process18,
+        surveyData.process19,
+        surveyData.process20,
+        surveyData.process21,
+        surveyData.process22,
+        surveyData.process23,
+        surveyData.frequency1,
+        surveyData.frequency2,
+        surveyData.frequency3,
+        surveyData.frequency4,
+        surveyData.frequency5,
+        surveyData.frequency6,
+        surveyData.frequency7,
+        surveyData.frequency8,
+        surveyData.frequency9,
+        surveyData.frequency10,
+        surveyData.firstOne,
+        surveyData.secondOne,
+        surveyData.thirdOne,
+        surveyData.fourthOne,
+        surveyData.situations1,
+        surveyData.situations2,
+        surveyData.situations3,
+        surveyData.situations4,
+        surveyData.situations5,
+        surveyData.situations6,
+        surveyData.situations7,
+        surveyData.situations8,
+        surveyData.situations9,
+        surveyData.situations10,
+        surveyData.situations11,
+        surveyData.situations12,
+        surveyData.situations13,
+        surveyData.situations14,
+        surveyData.situations15,
+        surveyData.situations16,
+        surveyData.situations17,
+        surveyData.situations18,
+        surveyData.situations19,
+        surveyData.situationsTwo1,
+        surveyData.situationsTwo2,
+        surveyData.situationsTwo3,
+        surveyData.situationsTwo4,
+        surveyData.situationsTwo5,
+        surveyData.situationsTwo6,
+        surveyData.family1,
+        surveyData.family2,
+        surveyData.family3,
+        surveyData.grade1,
+        surveyData.grade2,
+        surveyData.grade3,
+        surveyData.familyTwo1,
+        surveyData.familyTwo2,
+        surveyData.familyTwo3,
+        surveyData.friends1,
+        surveyData.friends2,
+        surveyData.friends3,
+        surveyData.someone1,
+        surveyData.someone2,
+        surveyData.someone3,
+        surveyData.someone4,
+        surveyData.someone5,
+        surveyData.someone6,
+        surveyData.memory1,
+        surveyData.memory2,
+        surveyData.memory3,
+        surveyData.memory4,
+        surveyData.memory5,
+        surveyData.memory6,
+        surveyData.memory7,
+        surveyData.memory8,
+        surveyData.memory9,
+        surveyData.memory10,
+        surveyData.memory11,
+        surveyData.memory12,
+        surveyData.daily1,
+        surveyData.daily2,
+        surveyData.daily3,
+        surveyData.daily4,
+        surveyData.daily5,
+        surveyData.daily6,
+        surveyData.daily7,
+        surveyData.daily8,
+        surveyData.daily9,
+        surveyData.daily10,
+        surveyData.daily11,
+        surveyData.daily12,
+        surveyData.daily13,
+        surveyData.daily14,
+        surveyData.daily15,
+        surveyData.daily16,
+        surveyData.daily17
+      ];
+      // Add the data to the first empty row
+      XLSX.utils.sheet_add_aoa(worksheet, [dataRow], { origin: firstEmptyRow, skipHeader: true });
+
+      // Update the range to include the new data
+      worksheet['!ref'] = XLSX.utils.encode_range(range.s, { c: range.e.c, r: firstEmptyRow });
+
+      // Write the updated workbook back to the file
+      const updatedExcelFileContent = XLSX.write(workbook, { type: 'base64' });
+      await FileSystem.writeAsStringAsync(filePath, updatedExcelFileContent, { encoding: FileSystem.EncodingType.Base64 });
+      console.log('Data added to Excel file.');
+
+      // Share the file with the user
+      await Sharing.shareAsync(filePath);
+
+    } catch (error) {
+      console.log('Error:', error);
+      Alert.alert('Error', 'No se pudo crear el archivo Excel o la carpeta.');
+    }
+  };
+
 
   console.log('Nombre del encuestado:', surveyData.nombre);
   console.log('Edad del encuestado:', surveyData.edad);
@@ -194,7 +449,7 @@ const LandingFinal = () => {
   console.log('Daily 15:', surveyData.daily15);
   console.log('Daily 16:', surveyData.daily16);
   console.log('Daily 17:', surveyData.daily17);
-  
+
   return (
     <View className="flex-1 bg-[#5F6896]">
       <View className="bg-[#FFFFFF] flex-row justify-center items-center p-20 rounded-b-[35]">
@@ -203,8 +458,8 @@ const LandingFinal = () => {
         <Image source={require('../../assets/images/UFRO.png')} className="w-[296] h-[102] rotate-180" />
       </View>
       <View className="flex-1 justify-center items-center">
-      <Text className="text-[#ffffff] text-6xl mx-5">Datos guardados</Text>
-        </View>
+        <Text className="text-[#ffffff] text-6xl mx-5">Datos guardados</Text>
+      </View>
     </View>
 
   );
