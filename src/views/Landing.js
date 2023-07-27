7a7229bea629e219f18a7a9be3f58bc51c834628
Nicolas <nicolasfelipegp@gmail.com>
2023-07-26 20:48:18 -0400

    Revirtiendo cambios

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,49 +1,10 @@
-import React from "react";
-import { Text, View, Image, Alert } from 'react-native';
+import {Text, View, Image } from 'react-native';
 import Button from "../components/Button";
 import { useNavigation } from "@react-navigation/native";
-import * as FileSystem from 'expo-file-system';
-import XLSX from 'xlsx';
-import * as Sharing from 'expo-sharing';
+
 
 export default function App() {
-  const navigation = useNavigation();
-
-  const createExcelFile = async () => {
-    const folderName = 'Propsy';
-    const fileName = 'propsy.xlsx';
-    const folderPath = `${FileSystem.documentDirectory}${folderName}`;
-    const filePath = `${folderPath}/${fileName}`;
-
-    try {
-      // Check if the file already exists
-      const fileExists = await FileSystem.getInfoAsync(filePath);
-
-      // If the file doesn't exist, create it and write the headers
-      if (!fileExists.exists) {
-        // Create the folder if it doesn't exist
-        await FileSystem.makeDirectoryAsync(folderPath, { intermediates: true });
-        console.log('Folder created:', folderPath);
-
-        const headers = [['nombre', 'edad', 'nacimiento', 'sexo', 'coincide']];
-        const worksheet = XLSX.utils.aoa_to_sheet(headers);
-        const workbook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
-        const excelFileContent = XLSX.write(workbook, { type: 'base64' });
-        await FileSystem.writeAsStringAsync(filePath, excelFileContent, { encoding: FileSystem.EncodingType.Base64 });
-        console.log('File created:', filePath);
-      }
-
-      // Share the file with the user
-      await Sharing.shareAsync(filePath);
-
-      // Now you can navigate to the next screen
-      navigation.navigate("Name");
-    } catch (error) {
-      console.log('Error:', error);
-      Alert.alert('Error', 'No se pudo crear el archivo Excel o la carpeta.');
-    }
-  };
+    const navigation = useNavigation();
 
   return (
     <View className="flex-1 bg-[#5F6896]">
@@ -53,8 +14,9 @@ export default function App() {
         <Image source={require('../../assets/images/UFRO.png')} className="w-[296] h-[102] rotate-180" />
       </View>
       <View className="flex-1 justify-center items-center">
-        <Button text="INICIAR" onPress={createExcelFile} />
-      </View>
+        <Button text="INICIAR" onPress={() => navigation.navigate("Name")} />
+        </View>
     </View>
+
   );
 }
